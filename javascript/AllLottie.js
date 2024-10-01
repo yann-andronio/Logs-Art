006c96968dd83a1a9415093130e58d4e2469d63b
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2024-10-01 21:41:09 +0300

    finition de header et integration de service

---
+++ javascript/AllLottie.js
@@ -0,0 +1,7 @@
+const animation = lottie.loadAnimation({
+    container: document.getElementById('lottieheader'), 
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+    path: 'assets/lottie/1.json' 
+});
